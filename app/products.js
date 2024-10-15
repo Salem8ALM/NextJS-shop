c4abcb243f12f84e4c09a47715bc032cd210e9e6
SALEM .. <salemsnsalmutairi@gmail.com>
2024-10-15 14:49:35 +0300

    page2

---
+++ app/products.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import ninteeneighty from "./nineteen.png";
+import animals from "./animalfarm.png";
+let products = [
+  {
+    name: "1984",
+    price: "19.84 kd",
+    image: animals,
+  },
+  {
+    name: "Animal farm",
+    price: "14.82 kd",
+    image: ninteeneighty,
+  },
+];
+export default products;
