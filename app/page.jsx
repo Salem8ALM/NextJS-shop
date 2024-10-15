3ede05e8647b67ec52c827e1cd4382686e78998c
SALEM .. <salemsnsalmutairi@gmail.com>
2024-10-15 13:51:40 +0300

    fixed everything

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,14 @@ import bookstoreFront from "./boogz.png";
 
 export default function Home() {
   return (
-    <div className=" pt-6 pb-6 text-center space-y-4 flex flex-col bg-white">
-      <h1 className="text-black font-extrabold text-xl">my bookstore</h1>
-      <p className="text-black font-semibold">get your banned books here</p>
-      <Image src={bookstoreFront} alt="" width={1000} height={500} />
+    <div className=" pt-6 pb-6 text-center space-y-4 flex flex-col justify-center bg-white">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-black font-extrabold text-xl">my bookstore</h1>
+        <p className="text-black font-semibold">get your banned books here</p>
+        <div className="flex flex-col justify-center">
+          <Image src={bookstoreFront} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
